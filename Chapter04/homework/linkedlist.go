91c0ed527df85733dd99620a085c43f4c3e3bad9
Bangchao Yu <yubc0321@gmail.com>
2017-12-20 16:19:36 +0800

    Update

---
+++ Chapter04/homework/linkedlist.go
@@ -0,0 +1,25 @@
+// Name: linkedlist.go
+// Func: 用Go语言实现单链表
+// Author: subond
+// Date: Dec 17, 2017
+package main
+
+import "fmt"
+
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
+
+// 创建
+func Add(l *ListNode, x []int) *ListNode {
+  if l == nil {
+    
+  }
+}
+
+func main() {
+  var mylist ListNode
+  var data = []int{1, 2, 3}
+  Add(mylist, data)
+}
