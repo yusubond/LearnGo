c63fe1ae8083cfc4241124120ee201541a57b7ac
Bangchao Yu <yubc0321@gmail.com>
2018-04-23 17:29:14 +0800

    Add main.go

---
+++ Package/src/subond/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"subond"
+)
+
+func main() {
+	// subond.MyLog()
+	// subond.OpenFile()
+	// subond.WordDictory()
+	// subond.NetPrint()
+	// subond.NetCIDR()
+	// subond.NetParseIP()
+	// subond.NetIPNet()
+
+	// myflag.go
+	// subond.Hello()
+
+	// myioutil.go
+	// subond.MyIO()
+	// suobnd.MyDir()
+	// subond.MyFile()
+
+	// myreflect.go
+	subond.MyReflect()
+}
